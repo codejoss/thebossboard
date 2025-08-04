2439a8dd32726af4c1c98420d6a074b2a86cf6b7
codejoss <quevedoportas@icloud.com>
2025-08-04 01:54:48 -0600

    feat: mejorar la gestión del modal de éxito y redirección
    Se implementó una nueva función para cerrar el modal de éxito y redirigir automáticamente a 'index.html' después de mostrar el modal. Además, se añadió un observador de mutaciones para gestionar la redirección automática tras la aparición del modal.

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -6,8 +6,23 @@ document.addEventListener('DOMContentLoaded', () => {
     new RegistrationManager();
 });
 
-// Función global para cerrar el modal de éxito
-window.closeSuccessModal = function() {
+// Función global para cerrar el modal de éxito y redirigir
+definirRedireccion();
+function definirRedireccion() {
+    window.closeSuccessModal = function() {
+        const modal = document.getElementById('success-modal');
+        modal.classList.add('hidden');
+        window.location.href = 'index.html';
+    };
+}
+
+// Redirección automática después de mostrar el modal de éxito
+const observer = new MutationObserver((mutations) => {
     const modal = document.getElementById('success-modal');
-    modal.classList.add('hidden');
-}; 
+    if (modal && !modal.classList.contains('hidden')) {
+        setTimeout(() => {
+            window.location.href = 'index.html';
+        }, 3000); // 3 segundos
+    }
+});
+observer.observe(document.body, { childList: false, subtree: true, attributes: true }); 
